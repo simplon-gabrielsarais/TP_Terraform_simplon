32c56d375ee682d897e07516111c825789879ad2
Simplon-gabrielsarais <99554412+simplon-gabrielsarais@users.noreply.github.com>
2023-09-04 14:02:36 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,38 @@
+# aks/main.tf
+
+provider "azurerm" {
+  features {}
+}
+
+module "network" {
+  source            = "../network"  # Chemin relatif vers le module réseau
+  resource_group_name = "my-aks-rg"
+  vnet_name           = "my-aks-vnet"
+  subnet_name         = "my-aks-subnet"
+}
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "example-aks-cluster"
+  location            = "East US"
+  resource_group_name = module.network.resource_group_name
+  dns_prefix          = "exampleaksdns"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin = "azure"
+    load_balancer_sku = "standard"
+  }
+}
+
+output "aks_cluster_id" {
+  value = azurerm_kubernetes_cluster.example.id
+}
